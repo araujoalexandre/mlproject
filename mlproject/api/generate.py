5e00fce117982adc45f01ce829c0b613542daf93
Alexandre <aaraujo001@gmail.com>
2017-05-30 20:20:51 +0200

    update api files

--- mlproject/api/generate.py
+++ mlproject/api/generate.py
@@ -12,28 +12,38 @@ __author__
     Araujo Alexandre < aaraujo001@gmail.com >
 
 """
+from os import makedirs
 from os.path import join, isfile
-from logging import getLogger, basicConfig, INFO
-from datetime.datetime import now
+from shutil import copyfile
+from datetime import datetime
 
-
-import os, sys
-import argparse
-import logging
-import random
-import datetime
-import shutil
-
-import pandas as pd
 import numpy as np
 
-from xgboost import DMatrix
-from sklearn.datasets import dump_svmlight_file
+try:
+    from pandas import DataFrame
+    PANDAS_INSTALLED = True
+except ImportError:
+    PANDAS_INSTALLED = False
 
-from mlproject.utils import to_print
+try:
+    from xgboost import DMatrix
+    XGBOOST_INSTALLED = True
+except ImportError:
+    XGBOOST_INSTALLED = False
+
+try:
+    from sklearn.datasets import dump_svmlight_file
+    SKLEARN_INSTALLED = True
+except ImportError:
+    SKLEARN_INSTALLED = False
+
+from mlproject.utils import print_and_log, init_log
 from mlproject.utils import pickle_load, pickle_dump
 from mlproject.utils import make_directory
 
+
+
+# XXX : get and save the shapes of train and test files in project folder ??
 
 class GenerateWrapper:
 
@@ -44,31 +54,22 @@ class GenerateWrapper:
         self.train_index, self.cv_index = None, None
         self.train_shape, self.test_shape = (None, None), (None, None)
         
-        self.log = self._init_logger()
+        self.log = init_log()
         self._print_params()
 
-        self.date = now().strftime("%Y.%m.%d %H.%M")
+        self.date = datetime.now().strftime("%Y.%m.%d %H.%M")
 
-
-    def _init_logger(self):
-        """
-            init logger for generate file
-        """
-        path = join('XXX', 'logs.log')
-        logger = getLogger()
-        basicConfig(filename=path, level=INFO)
-        return logger
+        self._save_custom = None
 
     def _folder_name(self):
         """
-            xxx
+            define name of folder to make
         """
-        timestamp = str(datetime.datetime.now().strftime("%Y.%m.%d_%H.%M"))
-        self.folder_name = 'model_{}'.format(timestamp)        
+        self.folder_name = 'model_{}'.format(self.date)        
 
     def _create_folder(self):
         """
-            xxx
+            function to make the folder
         """
         path = join(self.params.project_path, 'models', self.folder_name)
         make_directory(path)
@@ -77,7 +78,6 @@ class GenerateWrapper:
         """
             print global params
         """
-
         args_msg = [self.date,
                     self.folder_name,
                     self.nan_value,
@@ -90,26 +90,32 @@ class GenerateWrapper:
                     "FOLDS\t\t{}\n"
                     "SEED\t\t{}"
                 )
-        to_print(self.log, message.format(*args_msg))
+        print_and_log(self.log, message.format(*args_msg))
 
     def save_target(self, y):
         """
             save target in model folder
         """
         y_path = '{}/y_true.pkl'.format(self.folder_path)
-        if y is not None and not os.path.isfile(y_path):
+        if y is not None and not isfile(y_path):
             pickle_dump(y, y_path)
 
     def split_target(self):
         """
             split target array based on train and cv index
         """
+        # XXX : add check over self.train_index and self.cv_index
+        # XXX : add check over self.y_true => dim
+
         return self.y_true[self.train_index], self.y_true[self.cv_index]
 
     def split_weights(self):
         """
             split weight array based on train and cv index
         """
+        # XXX : add check over self.train_index and self.cv_index
+        # XXX : add check over self.weights => dim
+                    
         if self.weights is not None:
             return self.weights[self.train_index], self.weights[self.cv_index]
         return None, None
@@ -118,14 +124,60 @@ class GenerateWrapper:
         """
             split DataFrame array based on train and cv index
         """
+        # XXX : add check over self.train_index, self.cv_index
+        # XXX : add check over input DataFrame or Numpy array and dim
+        # XXX : handle absolute index and real index with loc / iloc method
+
         return df.loc[self.train_index].values, df.loc[self.cv_index].values
-    
-    def _dump_ffm(self, X, y, path):
+
+    def _save_xgb(self, X, y, weight, path):
         """
-            convert function to ffm format
+            function to convert and save : XGBoost format
         """
-        if isinstance(X, pd.DataFrame):
+        if not XGBOOST_INSTALLED:
+            raise Exception('Package XGBoost needs to be installed')
+        dxgb = DMatrix(X, y, missing=self.nan_value, weight=weight)
+        dxgb.save_binary(path)
+
+    def _save_pkl(self, X, y, weight, path):
+        """
+            function to convert and save : pickle format
+        """
+        pickle_dump(X, path)
+
+    def _save_csv(self, X, y, weight, path):
+        pass
+
+    def _save_npz(self, X, y, weight, path):
+        """
+            function to convert and save : compressed numpy format
+        """
+        np.savez_compressed(path, X)
+
+    def _save_libsvm(self, X, y, weight, path):
+        """
+            function to convert and save : libsvm format
+            format :
+
+                <label> <index>:<value> <index>:<value>
+        """
+        if not SKLEARN_INSTALLED:
+            raise Exception('Package Scikit-Learn needs to be installed')    
+        if y is None: y = np.zeros(len(X))
+        dump_svmlight_file(X, y, path)
+
+    def _save_libffm(self, X, y, weight, path):
+        """
+            function to convert and save : libffm format
+            format :
+
+                <label> <field>:<index>:<value> <field>:<index>:<value>
+        """
+        if y is None: y = np.zeros(len(X))
+        if PANDAS_INSTALLED and isinstance(X, DataFrame):
             X = X.values
+
+        # convert all to int ? what about NaN value ?
         X = np.array(X, dtype=np.int)
         with open(path, 'w') as f:
             for i, line in enumerate(X):
@@ -135,57 +187,44 @@ class GenerateWrapper:
                 out += '\n'
                 f.write(out)
 
-    def dump(self, X, y=None, weight=None, fold=None, type_='xgb', name='train'):
+    def dump(self, X, y, weight, fold, type_, name):
         """
             save X, y and weights in the right format
         """
+
+        # XXX : add check over type and name
 
         if fold is not None:
             dump_folder = 'fold_{}'.format(fold)
         else:
             dump_folder = 'test'
 
-        # create fold or test folder
+        # create fold_* or test folder
         path = join(self.folder_path, dump_folder)
-        if not os.path.exists(path):
-            os.makedirs(path)
+        make_directory(path)
 
+        # save y_train, y_cv => is it necessary ? 
         y_path = join(path, 'y_{}.pkl'.format(name))
         if y is not None and not isfile(y_path):
             pickle_dump(y, y_path)
 
+        # save w_train, w_cv => is it necessary ? 
         w_path = join(path, 'w_{}.pkl'.format(name))
         if weight is not None and not isfile(w_path):
             pickle_dump(weight, w_path)
 
         X_path = join(path, 'X_{}.{}'.format(name, type_))
-        if type_ == 'xgb':
-            if y is not None:
-                dxgb = DMatrix(X, y, missing=self.nan_value, weight=weight)
-            else:
-                dxgb = DMatrix(X, missing=self.nan_value, weight=weight)
-            dxgb.save_binary(X_path)
-        
-        elif type_ == 'pkl':
-            pickle_dump(X, X_path)
+        # switch 
+        {
+            'xgb':    self._save_xgb,
+            'pkl':    self._save_pkl,
+            'npz':    self._save_npz,
+            'libsvm': self._save_libsvm,
+            'libffm': self._save_libffm,
+            'csv':    self._save_csv,
+            'custom': self._save_custom,
 
-        elif type_ == 'libsvm':
-            if y is None:
-                y = np.zeros(len(X))
-            dump_svmlight_file(X, y, X_path)
-
-        elif type_ == 'npz':
-            np.savez_compressed(X_path, X)
-
-        elif type_ == 'libffm':
-            if y is None:
-                y = np.zeros(len(X))
-            self._dump_ffm(X, y, X_path)
-
-        elif type_ == 'custom':
-            if y is None:
-                y = np.zeros(len(X))
-            self.custom_dump(X, y, X_path)
+        }[type_](X, y, X_path)
 
     def cleaning(self, df):
         """
@@ -237,7 +276,6 @@ class GenerateWrapper:
             outfile.write('{0}\t{1}\tq\n'.format(i, feat))
         outfile.close()
 
-
     def conformity_test(self):
         """
             xxx
@@ -247,25 +285,24 @@ class GenerateWrapper:
         if self.train_shape[1] != self.test_shape[1]:
             message = "shape of DataFrames not equal : train {}, test {}"
             shapes = [self.train_shape[1], self.test_shape[1]]
-            to_print(self.log, message.format(*shapes))
+            print_and_log(self.log, message.format(*shapes))
             error = True
 
         if not np.array_equal(self.train_cols_name, self.test_cols_name):
-            to_print(self.log, "Columns of dataframes not equal")
+            print_and_log(self.log, "Columns of dataframes not equal")
 
         if error:
             a = set(self.train_cols_name)
             b = set(self.test_cols_name)
             diff_cols = a.symmetric_difference(b)
             for name in diff_cols:
-                to_print(self.log, "{}".format(name))
-
+                print_and_log(self.log, "{}".format(name))
     
     def save_infos(self):
         """
             save infos and files for training step
         """
-        to_print(self.log, "Dumping Info")
+        print_and_log(self.log, "Dumping Info")
         infos = {
             "seed": self.seed,
             "data_path": self.data_path,
@@ -288,7 +325,6 @@ class GenerateWrapper:
         path = join(self.folder_path, "validation.pkl")
         pickle_dump(self.validation, path)
 
-
     def copy_script(self):
         """
             backup dataset.py in folder model
@@ -296,5 +332,4 @@ class GenerateWrapper:
         for script_name in ["dataset.py"]:
             source = join(self.path, "code", script_name)
             destination = join(self.folder_path, script_name)
-            shutil.copyfile(source, destination)
-
+            copyfile(source, destination)
