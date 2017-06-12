44cb592cfe830aa02a7eaac1c062c7316545f40f
Alexandre <aaraujo001@gmail.com>
2017-06-12 22:33:10 +0200

    remove files

--- mlproject/data/submit.py
+++
@@ -1,9 +0,0 @@
-from pandas import DataFrame
-
-def submit(id_test, preds, args):
-    """
-        Function to create sumbit file for Kaggle competition
-    """
-    df_submit = DataFrame({'test_id':id_test, 'is_duplicate': preds})
-    file_name = "{}/submit_{}_{}_{:.5f}_0.00000.csv.gz".format(*args)
-    df_submit.to_csv(file_name, index=False, compression='gzip')
