a60ada39fb94ad6d99d9e6ec7b79b6b085805151
Alexandre <aaraujo001@gmail.com>
2017-06-13 12:22:59 +0200

    refactoring & reorganize unittests

--- tests/test_cmdline.py
+++ tests/test_cmdline.py
@@ -1,29 +1,33 @@
-
+import sys
 import unittest
 import glob
-from os.path import exists, join
-from tempfile import TemporaryDirectory
-from subprocess import Popen, PIPE
+from os.path import exists, join, abspath, dirname
+from shutil import copyfile
+from tempfile import TemporaryDirectory, mkdtemp
+from subprocess import Popen, PIPE, call
 from itertools import product
 
+from mlproject.utils import get_ext_cls
 
-class CmdlineTests(unittest.TestCase):
 
-    def _execute(self, cmd):
-        process = Popen(cmd, stdout=PIPE, bufsize=1, shell=True).communicate()
-        return process
+class BaseTests(unittest.TestCase):
 
-    def test_help(self):
-        cmd = "mlproject"
-        process = self._execute(cmd)
+    project_name = "proj123"
+
+    def _execute(self, *args, **kwargs):
+        cmd = (sys.executable, "-m", "mlproject.cmdline") + args
+        return Popen(cmd, stdout=PIPE, stderr=PIPE, **kwargs).communicate()
+
+
+class StartPojectTests(BaseTests):
 
     def test_starproject(self):
         with TemporaryDirectory() as path:
-            path = join(path, 'ml_project')
-            cmd = "mlproject startproject {}".format(path)
-            process = self._execute(cmd)
 
-            self.assertTrue(exists(join(path, 'code')))
+            p = self._execute("startproject", self.project_name, path)
+            path = join(path, self.project_name)
+
+            self.assertTrue(exists(join(path, 'code').strip()))
             self.assertTrue(exists(join(path, 'models')))
             self.assertTrue(exists(join(path, 'data')))
             self.assertTrue(exists(join(path, 'jupyter')))
@@ -31,30 +35,38 @@ class CmdlineTests(unittest.TestCase):
                 self.assertTrue(exists(join(path, 'data', 'train', folder)))
                 self.assertTrue(exists(join(path, 'data', 'test', folder)))
 
+class GenerateTest(BaseTests):
+
+    def setUp(self):
+        self.dir = TemporaryDirectory()
+        self._execute('startproject', self.project_name, self.dir.name)
+        self.path = join(self.dir.name, self.project_name)
+
+        scripts_dir = join(abspath(dirname(__file__)), 'scripts')
+        # override project file in code folder
+        src = join(scripts_dir, "binary.py")
+        copyfile(src, join(self.path, "code", "project.py"))
+
+    def tearDown(self):
+        self.dir.cleanup()
+
     def test_generate(self):
-        with TemporaryDirectory() as path:
 
-            # XXX : import this infos
-            extensions = ['xgb', 'npz', 'pkl']
+        extensions = list(get_ext_cls().keys())
+        # extensions = ['xgb', 'npz', 'pkl']
+        p = self._execute('generate', *extensions, cwd=join(self.path, "code"))
 
-            path = join(path, 'ml_project')
-            cmd = "mlproject startproject {} --test_code;".format(path)
-            cmd += "cd {};".format(join(path, "code"))
-            cmd += "mlproject generate {};".format(' '.join(extensions))
-            process = self._execute(cmd)
+        models_dirs = glob.glob(join(self.path, "models", "**"))
+        self.assertTrue(len(models_dirs))
 
-            folders = glob.glob(join(path, "models", "**"))
-            self.assertTrue(len(folders))
-
-            path = join(path, "models", folders[0])
-            gen = product(range(5), extensions, ['tr', 'va'])
-            for fold, ext, set_ in gen:
-                self.assertTrue(exists(join(path, "fold_{}".format(fold), 
-                    "X_{}.{}".format(set_, ext))))
-            for ext in extensions:
-                self.assertTrue(exists(join(path, 
-                        "test", "X_test.{}".format(ext))))
-
+        model = join(self.path, "models", models_dirs[0])
+        gen = product(range(5), extensions, ['tr', 'va'])
+        for fold, ext, set_ in gen:
+            self.assertTrue(exists(join(model, "fold_{}".format(fold), 
+                "X_{}.{}".format(set_, ext))))
+        for ext in extensions:
+            self.assertTrue(exists(join(model, 
+                    "test", "X_test.{}".format(ext))))
 
 if __name__ == '__main__':
     unittest.main()
