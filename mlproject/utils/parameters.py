7f414165cbf83e275917ac4c1dcbb83d2f07ee20
Alexandre <aaraujo001@gmail.com>
2017-06-14 00:10:10 +0200

    default value for unset attr

--- mlproject/utils/parameters.py
+++ mlproject/utils/parameters.py
@@ -9,7 +9,20 @@ class ParametersSpace:
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
-    # XXX : add check for mandatory
+
+        attributes = (
+            'project_path', 'data_path', 
+            'train_name', 'test_name'
+            'id_train', 'id_test',
+            'target_train', 'target_test',
+            'weights_train', 'weights_test',
+            'groups_train', 'groups_test',
+            'n_folds', 'seed','missing',
+        )
+        # set default attr if attr has not been set
+        for attr in attributes:
+            if not hasattr(self, attr):
+                setattr(self, key, None)
 
     def __repr__(self):
         return str(self.__dict__)
