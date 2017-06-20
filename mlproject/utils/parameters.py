bfa6c6ac067dcc8068e9fc7d18bc4493caa38591
Alexandre <aaraujo001@gmail.com>
2017-06-20 21:07:09 +0200

    Add assertion in ParamsSpace class

--- mlproject/utils/parameters.py
+++ mlproject/utils/parameters.py
@@ -3,26 +3,38 @@
 """
 
 from os.path import join
+import numpy as np
 
 class ParametersSpace:
 
     def __init__(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        
+        # required parameters
+        self.project_path  = kwargs.pop('project_path', None)
+        self.data_path     = kwargs.pop('data_path', None)
+        self.train_name    = kwargs.pop('train_name',None)
+        self.target_train  = kwargs.pop('target_train', None)
+        self.n_folds       = kwargs.pop('n_folds', None)
 
-        attributes = (
-            'project_path', 'data_path', 
-            'train_name', 'test_name'
-            'id_train', 'id_test',
-            'target_train', 'target_test',
-            'weights_train', 'weights_test',
-            'groups_train', 'groups_test',
-            'n_folds', 'seed','missing',
-        )
-        # set default attr if attr has not been set
-        for attr in attributes:
-            if not hasattr(self, attr):
-                setattr(self, key, None)
+        assert self.project_path, "Parameters are not configured correctly"
+        assert self.data_path   , "Parameters are not configured correctly"
+        assert self.train_name  , "Parameters are not configured correctly"
+        assert self.target_train, "Parameters are not configured correctly"
+        assert self.n_folds     , "Parameters are not configured correctly"
+
+        # optional parameters with default parameters
+        self.seed          = kwargs.pop('seed', 0)
+        self.missing       = kwargs.pop('missing', np.nan)
+
+        # optional parameters
+        self.test_name     = kwargs.get('test_name')
+        self.id_train      = kwargs.get('id_train')
+        self.id_test       = kwargs.get('id_test')
+        self.target_test   = kwargs.get('target_test')
+        self.weights_train = kwargs.get('weights_train')
+        self.weights_test  = kwargs.get('weights_test')
+        self.groups_train  = kwargs.get('groups_train')
+        self.groups_test   = kwargs.get('groups_test')
 
     def __repr__(self):
         return str(self.__dict__)
